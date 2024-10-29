e6fda392fe03fefb34143d91034272f029943a1e
Justin <justinguerrerowork@gmail.com>
2024-10-29 14:44:07 -0400

    menu

--- dashboard/src/app/(dashboard)/layout.tsx
+++ dashboard/src/app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
-// import Menu from "@/components/Menu";
-// import Navbar from "@/components/Navbar";
+import Menu from "@/components/Menu";
+import NavBar from "@/components/NavBar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,11 +19,11 @@ export default function DashboardLayout({
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold">Scholara</span>
         </Link>
-        {/* <Menu /> */}
+        <Menu />
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
-        {/* <Navbar /> */}
+        <NavBar />
         {children}
       </div>
     </div>
