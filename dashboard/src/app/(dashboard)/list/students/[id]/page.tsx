16c9768f0b01523f22fb1c4e2b296ba7ee9d1636
Justin <justinguerrerowork@gmail.com>
2024-10-31 22:41:32 -0400

    teacherpage query

--- dashboard/src/app/(dashboard)/list/students/[id]/page.tsx
+++ dashboard/src/app/(dashboard)/list/students/[id]/page.tsx
@@ -121,7 +121,7 @@ const SingleStudentPage = () => {
             <Link className="p-3 rounded-md bg-skylight" href="/">
               Student&apos;s Lessons
             </Link>
-            <Link className="p-3 rounded-md bg-purple" href="/">
+            <Link className="p-3 rounded-md bg-purple"href={`/list/teachers?classId=${2}`}>
               Student&apos;s Teachers
             </Link>
             <Link className="p-3 rounded-md bg-pink-50" href="/">
